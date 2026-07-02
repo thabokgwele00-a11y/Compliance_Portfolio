14aa4ac4d097785e7fb0e346cf33a02bff25e45b
thabokgwele00-a11y <thabokgwele00@gmail.com>
2026-07-02 16:37:47 +0200

    Delete Project 2 - Automated BA 900 Pipeline directory

--- Project 2 - Automated BA 900 Pipeline/Project2_Step3_Product_Mapping.sql
+++
@@ -1,5 +0,0 @@
-CREATE OR REPLACE TABLE `SARB_Reporting.Dim_Product_Mapping` AS
-SELECT 'SA01' AS Product_Code, 'Savings Account' AS Product_Name, 'Liquid' AS Liquidity_Status UNION ALL
-SELECT 'CH01', 'Cheque Account', 'Liquid' UNION ALL
-SELECT 'TD01', 'Term Deposit', 'Fixed' UNION ALL
-SELECT 'OD01', 'Overdraft', 'Credit'
